57b7c57805408ea24ee437d0800963d220c46cdf
jaehyeon-crema <jaehyeon.kim@cre.ma>
2025-04-23 21:00:59 +0900

    solve: 1493(1)

--- 1493.py
+++ 1493.py
@@ -6,6 +6,8 @@ class Solution:
         zero_index_arr = [-1] + zero_index_arr + [len(nums)]
         answer = 0
         for i in range(1, len(zero_index_arr)-1):
+            # 두 index a, b 사이의 간격은 a - b - 1
+            # 여기서는 i번째 숫자(0)를 삭제한 길이를 구하는것이라 a - b - 1 - 1 = a - b - 2
             length = zero_index_arr[i+1] - zero_index_arr[i-1] - 2
             answer = max(answer, length)
         return answer
