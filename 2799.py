df5dbcfe0b6c03bd7099e975f2251e6ca8215fc4
jaehyeonkim2358 <jaehyeonkim2358@gmail.com>
2025-04-24 22:11:09 +0900

    solve: 2799(2)

--- 2799.py
+++ 2799.py
@@ -1,26 +1,21 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        min_size = len(set(nums))
+        total_unique = len(set(nums))
+        count = 0
+        left = 0
+        freq = {}
 
-        answer = 1
-        for size in range(min_size, len(nums)):
-            num_count_map = {}
-            for i in range(size):
-                n = nums[i]
-                num_count_map[n] = num_count_map.get(n, 0) + 1
-            if len(num_count_map.keys()) >= min_size:
-                answer += 1
+        for right in range(len(nums)):
+            if nums[right] in freq.keys():
+                freq[nums[right]] = freq[nums[right]] + 1
+            else:
+                freq[nums[right]] = 1
 
-            for i in range(1, len(nums) - size + 1):
-                left_n = nums[i-1]
-                num_count_map[left_n] = num_count_map.get(left_n, 0) - 1
-                if num_count_map[left_n] <= 0:
-                    del(num_count_map[left_n])
+            while len(freq) == total_unique:
+                count += len(nums) - right
+                freq[nums[left]] -= 1
+                if freq[nums[left]] == 0:
+                    del freq[nums[left]]
+                left += 1
 
-                right_n = nums[i+size-1]
-                num_count_map[right_n] = num_count_map.get(right_n, 0) + 1
-
-                if len(num_count_map.keys()) >= min_size:
-                    answer += 1
-
-        return answer
+        return count
