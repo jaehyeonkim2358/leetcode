0bb2c738cd428ae401669b60db5abeb6c26e25b3
jaehyeonkim2358 <jaehyeonkim2358@gmail.com>
2025-04-30 04:21:48 +0900

    solve: 2962(2)

--- 2962.py
+++ 2962.py
@@ -1,16 +1,15 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
         max_num = max(nums)
-        prefix_sum = 0
-        prefix_sum_count = dict()
-        answer = 0
+        answer = prefix_sum = 0
+        prefix_sum_prefix_count = [0 for n in nums if n == max_num]
         for i in range(len(nums)):
-            n = nums[i]
-            if n == max_num:
-                prefix_sum_count[prefix_sum] = i # 새로운 최대값 전까지의 개수
+            if nums[i] == max_num:
+                prefix_sum_prefix_count[prefix_sum] = i + 1
                 prefix_sum += 1
 
-            if prefix_sum >= k:
-                answer += prefix_sum_count.get(prefix_sum - k, 0) + 1
+            if prefix_sum < k: continue
+
+            answer += prefix_sum_prefix_count[prefix_sum - k]
 
         return answer
