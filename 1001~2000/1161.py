5e5793f36ee34a83f029fefd2844f32adf899917
jaehyeonkim2358 <jaehyeonkim2358@gmail.com>
2025-05-14 21:40:02 +0900

    solve: 1161(2)

--- 1001~2000/1161.py
+++ 1001~2000/1161.py
@@ -1,5 +1,3 @@
-from queue import PriorityQueue
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -8,14 +6,13 @@ from queue import PriorityQueue
 #         self.right = right
 class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-        counter = itertools.count()
-        queue = [(1, next(counter), root)]
+        queue = deque([(1, root)])
         pre_depth = 1
         max_sum = float('-inf')
         answer = 1
         depth_sum = root.val
         while queue:
-            depth, _, node = heappop(queue)
+            depth, node = queue.popleft()
             if depth > pre_depth:
                 if depth_sum > max_sum:
                     max_sum = depth_sum
@@ -24,8 +21,8 @@ class Solution:
                 depth_sum = 0
             depth_sum += node.val
             if node.right:
-                heappush(queue, (depth + 1, next(counter), node.right))
+                queue.append((depth + 1, node.right))
             if node.left:
-                heappush(queue, (depth + 1, next(counter), node.left))
+                queue.append((depth + 1, node.left))
 
         return pre_depth if depth_sum > max_sum else answer
