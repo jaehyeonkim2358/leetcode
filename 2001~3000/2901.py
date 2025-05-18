a999035b9954d84833ae97f0d4f620c0495c0c83
jaehyeonkim2358 <jaehyeonkim2358@gmail.com>
2025-05-18 16:01:41 +0900

    solve: 2901(1)

--- 2001~3000/2901.py
+++ 2001~3000/2901.py
@@ -2,16 +2,20 @@ class Solution:
     def getWordsInLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
         N = len(words)
         dp = [0] * N
-        parent = [-1] * N
+        parent = [-1] * N # parent[i]에 subsequence를 구성하는 words[i] 이전 요소의 index를 저장
         max_index = 0
         for i in range(N):
             for j in range(i):
                 if self.check(i, j, words, groups) and dp[i] < dp[j] + 1:
                     dp[i] = dp[j] + 1
+
+                    # words[i]가 현재 까지 만들어진 subsequence의 마지막 요소일 때,
+                    # subsequence의 words[i] 이전 값은 words[j]
                     parent[i] = j
                 if dp[i] > dp[max_index]:
                     max_index = i
 
+        # 마지막 요소(words[max_index])부터 시작해서 parent[i]에 저장된 index를 따라 이전 요소를 찾아가면서 전체 subsequence를 구성
         answer = []
         index = max_index
         while index >= 0:
