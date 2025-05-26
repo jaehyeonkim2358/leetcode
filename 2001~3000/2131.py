91a1cb7f4a1d56b06cf6139a31eabb56083e1c5d
jaehyeonkim2358 <jaehyeonkim2358@gmail.com>
2025-05-26 21:30:28 +0900

    solve: 2131(2)

--- 2001~3000/2131.py
+++ 2001~3000/2131.py
@@ -5,14 +5,19 @@ class Solution:
         additional_count = 0
         for w, cnt in counter.items():
             reversed_w = w[::-1]
-            if w == reversed_w:             # 앞뒤가 같은 문자('gg', 'mm')
+
+            # 앞뒤가 같은 문자('gg', 'mm')
+            if w == reversed_w:
                 if cnt >= 2:
-                    usable_count = (cnt // 2) * 2
+                    usable_count = cnt - (cnt % 2)
                     answer += usable_count * 2
                     counter[w] -= usable_count
                 if counter[w] > 0:
+                    # '앞뒤가 같은 문자'하나를 팰린드롬의 가운데에 끼워넣을 수 있음
                     additional_count = 1
-            elif counter[reversed_w] > 0:   # 앞뒤가 다른 문자('el', 'fc')
+
+            # 앞뒤가 다른 문자('el', 'fc')
+            elif counter[reversed_w] > 0:
                 answer += min(counter[w], counter[reversed_w]) * 2
 
         return answer + additional_count * 2
