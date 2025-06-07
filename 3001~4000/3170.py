25ce18c02f3f3c5bc77a3bb4bd31e58ad9d4b772
jaehyeonkim2358 <jaehyeonkim2358@gmail.com>
2025-06-07 16:01:21 +0900

    solve: 3170(2)

--- 3001~4000/3170.py
+++ 3001~4000/3170.py
@@ -4,17 +4,22 @@ class Solution:
     def clearStars(self, s: str) -> str:
         if '*' not in s: return s
 
+        ord_a = ord('a')
         N = len(s)
-        queue = []
+        alph_queue = []
+        stack = [[] for _ in range(26)]
         remove = set()
         for i in range(N):
             ch = s[i]
             if ch == '*':
                 remove.add(i)
-                if queue:
-                    _, index = heappop(queue)
-                    remove.add(-index)
+                if alph_queue:
+                    alph_num = heappop(alph_queue)
+                    index = stack[alph_num].pop()
+                    remove.add(index)
             else:
-                heappush(queue, (ch, -i))
+                alph_num = ord(ch) - ord_a
+                heappush(alph_queue, alph_num)
+                stack[alph_num].append(i)
 
         return ''.join([s[i] for i in range(N) if i not in remove])
