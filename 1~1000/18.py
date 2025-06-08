da1916d3bd691c69a74b13420e43be626f3c52ec
jaehyeonkim2358 <jaehyeonkim2358@gmail.com>
2025-06-08 17:08:32 +0900

    solve: 18(2)

--- 1~1000/18.py
+++ 1~1000/18.py
@@ -1,23 +1,30 @@
-from collections import Counter
-
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        counter = Counter(nums)
-        ans = set()
-        N = len(nums)
-        for i in range(N-3):
-            for j in range(i+3, N):
-                left, right = i+1, j-1
-                while left < right:
-                    s = nums[i] + nums[j] + nums[left] + nums[right]
-                    if s < target:
-                        left += counter[nums[left]]
-                    elif s > target:
-                        right -= counter[nums[right]]
-                    else:
-                        ans.add((nums[i], nums[j], nums[left], nums[right]))
-                        left += counter[nums[left]]
-                        right -= counter[nums[right]]
+        ans = []
+        self.ksum(0, len(nums)-1, 4, target, [], ans, nums)
+        return ans
 
-        return list(ans)
+    def ksum(self, l, r, k, target, path, out, nums):
+        def skipSameNum(idx, nums):
+            while idx+1 < len(nums) and nums[idx] == nums[idx+1]: idx += 1
+            return idx+1
+
+        if k == 2:
+            while l < r:
+                s = nums[l] + nums[r]
+                if s < target:
+                    l += 1
+                elif s > target:
+                    r -= 1
+                else:
+                    out.append(path + [nums[l], nums[r]])
+                    l = skipSameNum(l, nums)
+                    r -= 1
+            return
+
+        while l < r:
+            path.append(nums[l])
+            self.ksum(l+1, r, k-1, target - nums[l], path, out, nums)
+            path.pop()
+            l = skipSameNum(l, nums)
