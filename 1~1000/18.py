ce12d2590543cb38d727ae6620783d3a7a51f828
jaehyeonkim2358 <jaehyeonkim2358@gmail.com>
2025-06-08 16:36:46 +0900

    solve: 18(1)

---
+++ 1~1000/18.py
@@ -0,0 +1,21 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        counter = Counter(nums)
+        ans = set()
+        N = len(nums)
+        for i in range(N-3):
+            for j in range(i+3, N):
+                left, right = i+1, j-1
+                while left < right:
+                    s = nums[i] + nums[j] + nums[left] + nums[right]
+                    if s < target:
+                        left += counter[nums[left]]
+                    elif s > target:
+                        right -= counter[nums[right]]
+                    else:
+                        ans.add((nums[i], nums[j], nums[left], nums[right]))
+                        left += counter[nums[left]]
+                        right -= counter[nums[right]]
+
+        return list(ans)
