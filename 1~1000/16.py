9ae7024098ae771c14580be4f56eda234b3b4b33
jaehyeonkim2358 <jaehyeonkim2358@gmail.com>
2025-06-08 16:09:41 +0900

    solve: 16(2)

--- 1~1000/16.py
+++ 1~1000/16.py
@@ -1,35 +1,19 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        min_gap = float('inf')
-        ans = 0
+        N = len(nums)
         nums.sort()
-        for i in range(len(nums)-2):
-            for j in range(i+2, len(nums)):
-                val = nums[i] + nums[j]
-                new_sum = self.bi_search(nums, target, val, min_gap, i, j)
-                new_gap = abs(target - new_sum)
+        min_gap = float('inf')
+        for i in range(N-2):
+            left, right = i+1, N-1
+            while left < right:
+                s = nums[i] + nums[left] + nums[right]
+                if s > target:
+                    right -= 1
+                else:
+                    left += 1
+                new_gap = abs(s - target)
                 if new_gap < min_gap:
                     min_gap = new_gap
-                    ans = new_sum
-        return ans
+                    ans = s
 
-    def bi_search(self, nums, target, val, min_gap, i, j):
-        s = i+1
-        e = j-1
-        m = (s + e) // 2
-        while s < e:
-            m = (s + e) // 2
-            if nums[m] + val >= target:
-                e = m
-            else:
-                s = m + 1
-        v1 = nums[s] + val
-        v2 = nums[m] + val
-        v3 = nums[e] + val
-        min_gap = min(abs(target-v1), abs(target-v2), abs(target-v3))
-        if min_gap == abs(target-v1):
-            return v1
-        elif min_gap == abs(target-v2):
-            return v2
-        else:
-            return v3
+        return ans
