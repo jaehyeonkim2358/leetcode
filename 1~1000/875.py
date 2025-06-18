4cf539cce7a122b819713f99f2d04243ae431135
jaehyeonkim2358 <jaehyeonkim2358@gmail.com>
2025-06-18 22:14:35 +0900

    solve: 875(2)

--- 1~1000/875.py
+++ 1~1000/875.py
@@ -1,17 +1,9 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def canEatAll(k):
-            total = 0
-            for p in piles:
-                # `p + alpha`가 k로 나누어 떨어지도록 하는 alpha값을 계산
-                alpha = (k - (p % k)) % k
-                total += p + alpha
-            return total // k <= h
-
         s, e = 1, max(piles)
         while s < e:
             m = (s+e)//2
-            if canEatAll(m):
+            if sum(math.ceil(p / m) for p in piles) <= h:
                 e = m
             else:
                 s = m + 1
